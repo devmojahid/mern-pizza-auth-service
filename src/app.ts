51221c433c69beae0d499b9059da959b56ddc2cd
Mojahid Islam <raofahmedmojahid@gmail.com>
2024-02-24 22:26:16 +0600

    error handler adding

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
+import createError from "http-errors";
 
 const app = express();
 
 app.get("/", (req, res) => {
+    const err = createError(404, "This video does not exist!");
+    throw err;
     res.send("Auth System Hear");
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
